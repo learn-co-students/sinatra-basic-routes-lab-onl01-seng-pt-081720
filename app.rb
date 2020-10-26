771b6b33121a7578ecc1dedf0b2d536e3ec2f281
alexcoldbrew <alexcoldbrew@gmail.com>
2020-10-25 23:18:12 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Alex"
+    end
+
+    get '/hometown' do
+        "My hometown is Lake Jackson"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Gucci Gang"
+    end
+
 end
