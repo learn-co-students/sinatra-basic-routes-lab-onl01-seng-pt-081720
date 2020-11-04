fa84756149235c0c787fac90b59a8d939f9f8c9c
Brenda Ferrufino <ferrufinob@gmail.com>
2020-11-04 13:38:30 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Brenda"
+    end
+
+    get '/hometown' do
+        "My hometown is Washington, DC"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Sway by Michael Buble"
+    end
 end
