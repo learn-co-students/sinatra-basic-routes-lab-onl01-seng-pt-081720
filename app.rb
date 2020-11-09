1a468f675e9e7c18ab289093a2b43087d8b10ac2
TheodoreSchafer <theodore.schafer@gmail.com>
2020-11-09 10:01:11 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do
+        "My name is Theodore."
+    end
+
+    get '/hometown' do
+        "My hometown is Pinckney, Michigan."
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Meta Concrete by Rene Hell."
+    end 
+
+
 end
