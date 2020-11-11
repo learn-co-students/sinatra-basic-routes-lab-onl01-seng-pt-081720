e20788a4f221b6fc16a90bef1c5b99046ca23da5
taylorsieling <taylorasieling@gmail.com>
2020-11-11 11:28:58 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Taylor."
+    end
+
+    get '/hometown' do
+        "My hometown is League City, Texas."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Dips by Daisy the Great."
+    end
+
 end
