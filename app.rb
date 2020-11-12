371ae2caef163664e13d061954d30e547acae7e4
Angelica203 <brantleyhomebuyer@gmail.com>
2020-11-12 13:48:54 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+     "Hello World!"    
+    end
+
+    get '/name' do
+        "My name is Angelica."
+    end
+
+    get '/hometown' do
+        "My hometown is New Haven."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Infatuated."
+    end
 end
