ba12645f7fe0e109529bea386f49dc203999fc1e
knbrlo <kenbrlow@gmail.com>
2020-12-07 22:35:18 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Ken."
+    end
+
+    get '/hometown' do
+        "My hometown is St. George, UT."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is High Hopes by Panic! At The Disco"
+    end
 end
